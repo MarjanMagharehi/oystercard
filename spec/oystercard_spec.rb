4e848a155eae98430bb40e88cb16cce0cf94bcbb
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-07 10:38:37 +0000

    Add test for maximum balance top-up error

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,11 @@ describe Oystercard do
   end
   describe '#top_up' do
     it{is_expected.to respond_to(:top_up).with(1).argument}
+    it 'tops up the balance of the oystercard' do
+      expect{subject.top_up 1}.to change{subject.balance}.by 1
+    end
+    it 'raises an error if the maximum balance is exceeded' do
+      expect{subject.top_up(91)}.to raise_error "Top up failed, maximum balance exceeded"
+    end 
   end
-
 end

d172913af47a81dc2dcbe95e8022a975330b6d9e
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-07 11:13:10 +0000

    Add maximum balance constant to oystercard file

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,10 @@
 class Oystercard
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
   def initialize
     @balance = 0
   end
   def top_up(amount)
-    @balance += amount 
+    @balance += amount
   end
 end

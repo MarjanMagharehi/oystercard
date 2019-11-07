1a7a9625d80857da2324c9f9e0618cd63f2fdb33
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-07 11:15:18 +0000

    Add code to pass raise error test, with if statementto raise the maximum balance error

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,6 +5,7 @@ class Oystercard
     @balance = 0
   end
   def top_up(amount)
+    fail "Top up failed, maximum balance exceeded" if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 end
